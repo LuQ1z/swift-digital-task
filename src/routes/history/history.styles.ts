62c37589e7fef5df2b425b19042004607d74775b
Kerdze <mehgaming772@gmail.com>
2024-03-03 14:18:13 +0400

    feat: started history page

---
+++ src/routes/history/history.styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	background-color: whitesmoke;
+	border: 2px solid grey;
+	border-radius: 16px;
+	padding: 2rem;
+	gap: 1rem;
+`;
